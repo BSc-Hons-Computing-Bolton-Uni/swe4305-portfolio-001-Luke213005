efd5eba7f594677b752d3bb1e668608c0af45607
Luke213 <100182377@student.burnley.ac.uk>
2025-01-28 15:35:09 +0000

    Task 2

--- Logbook/Week1/Main.java
+++ Logbook/Week1/Main.java
@@ -6,10 +6,17 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Enter your name: ");
-        String name = scanner.nextLine();
+        System.out.print("Enter the length of the rectangle: ");
+        double length = scanner.nextDouble();
 
-        System.out.println("Hello " + name + "!");
+        System.out.print("Enter the height of the rectangle: ");
+        double height = scanner.nextDouble();
+
+        double perimeter = 2 * (length + height);
+        double area = length * height;
+
+        System.out.println("The perimeter of the rectangle is: " + perimeter);
+        System.out.println("The area of the rectangle is: " + area);
 
         scanner.close();
     }
